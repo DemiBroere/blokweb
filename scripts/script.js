c0fefb5ac65c3a43b18a805fabe1669b91924ff2
Demi Broere <90050856+DemiBroere@users.noreply.github.com>
2021-09-20 21:53:43 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+/* Menu. */
+
+var menuButton = document.querySelector("button");
+var menuSee = document.querySelector("header nav ul");
+
+function menuVerschijnt() {
+    menuSee.classList.toggle("terug")
+}
+
+menuButton.addEventListener("click", menuVerschijnt);
