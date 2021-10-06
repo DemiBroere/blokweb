8ffb4ce59f2d98ed002028b283084875519be108
Demi Broere <90050856+DemiBroere@users.noreply.github.com>
2021-10-06 17:05:33 +0200

    Delete basiswebsite directory

--- basiswebsite/scripts/script.js
+++
@@ -1,8 +0,0 @@
-var menuButton = document.querySelector("nav button");
-var menuSee = document.querySelector("nav ul");
-
-function toonMenu() {
-    menuSee.classList.toggle("terug");
-}
-
-menuButton.addEventListener("click", toonMenu);
